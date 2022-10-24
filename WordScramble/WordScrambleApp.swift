e53aac39738db1e9d6c8631fbef1a85cf9cd1700
Matilda Mared <matildamared@live.se>
2022-10-24 10:06:29 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Matilda Mared on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
